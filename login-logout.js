230871f522bd19ff73c2643bd0c5f7c8c188e3a1
crystallewisdeveloper <107262091+crystallewisdeveloper@users.noreply.github.com>
2025-03-03 15:46:37 -0500

    login game v2

--- login-logout.js
+++ login-logout.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     const SUPABASE_KEY =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrYWVxcXF4aGtnb3NmcHB6bW10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQyNzEyMjgsImV4cCI6MjA0OTg0NzIyOH0.dpxd-Y6Zvfu_1tcfELPNV7acq6X9tWMd8paNK28ncsc";
 
-    const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+    const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
     const toggleBtn = document.querySelector("#auth-toggle-btn");
 
     if (!toggleBtn) {
